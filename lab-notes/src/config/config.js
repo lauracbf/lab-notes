9420eb8c8a122ec0d870a049bcfaa3aa9e8fbb96
lauracbf <53447565+lauracbf@users.noreply.github.com>
2020-03-30 12:54:32 -0600

    Update config.js

--- lab-notes/src/config/config.js
+++ lab-notes/src/config/config.js
@@ -1,10 +1,4 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyB1gCrtZTfAK49K1ZSSu3DniHPDyoBfupI",
-    authDomain: "react-notes-1fde4.firebaseapp.com",
-    databaseURL: "https://react-notes-1fde4.firebaseio.com",
-    projectId: "react-notes-1fde4",
-    storageBucket: "react-notes-1fde4.appspot.com",
-    messagingSenderId: "980808157365",
-    appId: "1:980808157365:web:9f7b835f92f3a9459a96fc"
+ 
   };
 
